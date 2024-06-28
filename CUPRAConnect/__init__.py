cf316c3832a1d895bccf5af156389960eb70d6b0
Jumphrey1670 <josh12345671@live.co.uk>
2024-06-28 21:51:05 +0100

    Fixing Stuff

---
+++ CUPRAConnect/__init__.py
@@ -0,0 +1,7 @@
+"""
+CUPRAConnect - A Python 3 library for interacting with CUPRA Connect - Fork from https://github.com/farfar/seatconnect .
+
+For more details and documentation, visit the github page at https://github.com/farfar/seatconnect
+"""
+
+from CUPRAConnect.connection import Connection
